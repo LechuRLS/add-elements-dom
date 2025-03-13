b536baa3b5a513d678119e9545a751cbd7cea14b
Lechu Rules <hugogarrui@gmail.com>
2025-03-13 13:22:34 +0100

    add index html

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
  // Aquí tu código
+ const agregar = document.getElementById("agregar")
+ const lista = document.getElementById("lista")
+ const borrar = document.getElementById("borrar")
+
+ function nuevoElemento() {
+    let nuevoElemento = prompt("Introduce aqui un elemento");
+    if (nuevoElemento !== null && nuevoElemento.trim() !== ""){
+        let li = document.createElement("li");
+        li.textContent = nuevoElemento;
+        lista.appendChild(li);
+    } else {
+        alert("Elemento inválido")
+    }
+}
+
+    function borrarLista() {
+        lista.innerHTML = "";
+    }
+
+    agregar.addEventListener("click", nuevoElemento)
+    borrar.addEventListener("click", borrarLista)
